d9fa53c04f593518ec992913b9137bf5336240c8
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-03-14 10:25:39 +0100

    removing virtual peering

--- main.tf
+++ main.tf
@@ -15,15 +15,15 @@ resource "azurerm_virtual_network" "this" {
   tags = merge(var.default_tags, var.extra_tags)
 }
 
-resource "azurerm_virtual_network_peering" "this" {
-  for_each                     = var.peered_vnets != null ? var.peered_vnets : []
-  name                         = format("peering-to-%s", each.value.remote_vnet_name)
-  resource_group_name          = azurerm_virtual_network.this.resource_group_name
-  virtual_network_name         = azurerm_virtual_network.this.name
-  remote_virtual_network_id    = each.value.remote_vnet_id
-  allow_virtual_network_access = try(each.value.allow_virtual_network_access, true)
-  allow_forwarded_traffic      = try(each.value.allow_forwarded_traffic, true)
-  allow_gateway_transit        = try(each.value.allow_gateway_transit, true)
-  use_remote_gateways          = try(each.value.use_remote_gateways, true)
-  #triggers                     = try(each.value.triggers, true)
+#resource "azurerm_virtual_network_peering" "this" {
+#  for_each                     = var.peered_vnets != null ? var.peered_vnets : []
+#  name                         = format("peering-to-%s", each.value.remote_vnet_name)
+#  resource_group_name          = azurerm_virtual_network.this.resource_group_name
+#  virtual_network_name         = azurerm_virtual_network.this.name
+#  remote_virtual_network_id    = each.value.remote_vnet_id
+#  allow_virtual_network_access = try(each.value.allow_virtual_network_access, true)
+#  allow_forwarded_traffic      = try(each.value.allow_forwarded_traffic, true)
+#  allow_gateway_transit        = try(each.value.allow_gateway_transit, true)
+#  use_remote_gateways          = try(each.value.use_remote_gateways, true)
+#  #triggers                     = try(each.value.triggers, true)
 }
